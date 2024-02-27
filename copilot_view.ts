7008d280ab170f3ebb29fc29999dc289bf850ed7
KTK27 <kartiksharmahkg852@gmail.com>
2024-02-27 20:28:28 +0800

    Polish the UI, add Dark/Light Mode which adapts to system

--- copilot_view.ts
+++ copilot_view.ts
@@ -21,17 +21,29 @@ export class CopilotView extends ItemView {
     async onOpen() {
         // create the frame for this link https://edgeservices.bing.com/edgesvc/shell
 
+
+        // check if the class theme-dark exists
+        console.log(document.body.classList);
+        const theme = document.body.classList.contains("theme-dark") ? "&darkschemeovr=1" : "";
+        const url = "https://edgeservices.bing.com/edgesvc/chat?udsframed=1&form=SHORUN&clientscopes=chat,noheader,channelstable,&setlang=en-US" + theme
+
+
         const container = this.containerEl.children[1];
         container.empty();
-        container.createEl("h1", { text: "Bing Copilot for Obsidian" });
-        const iframe = container.createEl("iframe", {
+        const div = container.createEl("div", {
             attr: {
-                src: "https://edgeservices.bing.com/edgesvc/chat?udsframed=1&form=SHORUN&clientscopes=chat,noheader,channelstable,&shellsig=6de7da44d1b3f7278f74106f0aded8f7e5ff69a9&setlang=en-US&darkschemeovr=1",
+                style: "width: 100%; height: 100%; border-radius: 10px; margin: 0px; padding: 0px; overflow: hidden;"
+            }
+        });
+        const iframe = div.createEl("iframe", {
+            attr: {
+                src: url,
                 style: "width: 100%; height: 100%;",
                 id: "underside-iframe-container",
                 name: "underside-iframe-container",
                 frameborder: "0",
-                allow: "clipboard-write"
+                allow: "clipboard-write",
+
 
             }
         });
